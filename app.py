19018631d9bb8635492de4309b78cac871f7d817
crislopes3005 <148553965+crislopes3005@users.noreply.github.com>
2026-04-28 16:45:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -232,14 +232,14 @@ st.plotly_chart(fig1, use_container_width=True)
 # 🔹 Propostas por dia
 st.subheader("Propostas por dia")
 
-propostas_dia = df_propostas.groupby('data_publicacao')['id'].count().reset_index()
+propostas_dia = df_propostas.groupby('data_publicacao')['id_proposta'].count().reset_index()
 
 fig2 = px.line(
     propostas_dia.sort_values('data_publicacao'),
     x='data_publicacao',
-    y='id',
+    y='id_proposta',
     markers=True,
-    text='id',
+    text='id_proposta',
     color_discrete_sequence=[COR_PRINCIPAL]
 )
 
